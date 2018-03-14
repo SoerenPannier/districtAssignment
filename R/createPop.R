477c65c0a1d36f81129c6432c7dd514601d845a6
Pannier <spannier@fu-berlin.de>
2018-03-14 15:34:41 +0100

    comments

--- R/createPop.R
+++ R/createPop.R
@@ -1,22 +1,20 @@
-createPop <- function( seed = NULL,
-                       expension = 1,
-                       dispersion = 0,
-                       data)
+createPop <- function(seed = NULL,
+                      expension = 1,
+                      dispersion = 0,
+                      data)
 {
+  # creating a fresh seed if none was selected for assuring reproducibility
   if (is.null(seed)) {
     seed <- rbinom(n = 1, size = 100, prob = runif(1))
   }
   set.seed(seed)
-  #library(laeken)
-  # a_0 <- readRDS(file = "./Data/AUT_adm0.rds")
-  # a_1 <- readRDS(file = "./Data/AUT_adm1.rds")
-  
+
   load(file = "./Data/shape_austria_dis.rds")
   a_2 <- shape_austria_dis
-  a2_ids <- (cbind(a_2$ST, a_2$BL, a_2$disChar))
+  a2_ids <- cbind(a_2$ST, a_2$BL, a_2$disChar)
   eusilcP <- NULL
   
-  
+  # combining english and german state names
   my_eusilc <- data[data$main, ]
   my_eusilc$eqIncome <- as.numeric(my_eusilc$eqIncome)
   map_tab <- data.frame(deutsch = c("Burgenland",
@@ -41,23 +39,28 @@ createPop <- function( seed = NULL,
   my_eusilc <- merge(x = my_eusilc, y = map_tab, by.x = "region", 
                      by.y = "englisch", all.x = T)
   
+  # expanding the population
   if (expension > 1) {
-    expnd  <- sample(x = seq_len(nrow(my_eusilc)), replace = TRUE, size = trunc(nrow(my_eusilc) * expension))
+    expnd  <- sample(x = seq_len(nrow(my_eusilc)), replace = TRUE, 
+                     size = trunc(nrow(my_eusilc) * expension))
     my_eusilc <- my_eusilc[expnd, ]
   }
   
   full_smps <- table(my_eusilc$region)
   
+  # obtain percentage populationsizes and regional net income
   source("./R/percentages.R")
   regions <- getPercentages()
   
+  # order regions by their state and net income per capita
   regions <-  regions[order(regions$State.y, 
                             regions$netPerCapita, 
                             decreasing = T), ] 
   rangOrder <- regions$District
   regions$state <- as.character(regions$State.x)
-  # runif(n = nrow(regions)))
-  
+
+  # selecting population sizes for each region based on their percentage of the 
+  # states population
   dom_sizes <-  function(size, name) {
     dom_s <- quantile(seq_len(size), probs = 
                         cumsum(regions$percentage[regions$state == name]), 
@@ -79,7 +82,8 @@ createPop <- function( seed = NULL,
   my_eusilc$region <- as.character(my_eusilc$region)
   
   incomeOrder <- my_eusilc$eqIncome
-  
+  # add some dispersion on the income in order to achieve a realistic
+  # inter cluster correlation (ICC)
   if (is.numeric(dispersion) && dispersion != 0)
   {
     incomeOrder <- incomeOrder + rnorm(n = length(incomeOrder),
@@ -89,9 +93,8 @@ createPop <- function( seed = NULL,
   
   my_eusilc <- my_eusilc[order(my_eusilc$region, incomeOrder, decreasing = T),]
   sub_doms <- sub_doms[order(sub_doms$state, decreasing = T),]
-  sum(sub_doms$count)
-  nrow(my_eusilc)
-  
+
+  # adding the district variable to the population dataset
   my_eusilc$sub_2 <- rep(sub_doms$district, sub_doms$count)
   
   return( list(seed = seed, 
--- R/generationAndSampling.R
+++ R/generationAndSampling.R
@@ -3,24 +3,34 @@
 library(sampling)
 library(simFrame)
 library(maptools)
+
+# load utility functions
+# functions to reduce the number of and rename columns 
 source("./R/adjustDataSets.R")
+#function to create population data
 source("./R/createPop.R")
+
 data("eusilcP")
+# creation of a population
 test <- createPop(seed = 96, expension = 8, dispersion = 1, data = eusilcP)
 
 dim(test$eusilcHHpop)
 
 summary(as.numeric(table(test$eusilcHHpop$sub_2)))
+
+# choosing initial sample sizes
 distSizes <- as.numeric(table(test$eusilcHHpop$sub_2))
 smpSizes <- ifelse(distSizes <= quantile(distSizes, 0.25), 1, 100)
 
-length(unique(test$eusilcHHpop$sub_2))
-
+# using the 25% smallest domains as out of sample
 tempDf <- test$eusilcHHpop[!test$eusilcHHpop$sub_2 %in% 
-                             names(table(test$eusilcHHpop$sub_2))[ distSizes <= quantile(distSizes, 0.25)],]
+                             names(table(test$eusilcHHpop$sub_2))[ distSizes <= 
+                                                                     quantile(distSizes, 0.25)],]
 tempDf$sub_2 <- factor(tempDf$sub_2, levels = unique(tempDf$sub_2))
+# truncating sample sizes of remaining domains, at 1 and 200 respectively
 smpSizes <- apply(cbind(ceiling(as.numeric(table(tempDf$sub_2)) * 0.025), 200), 1, min)
 
+# stratified sampling
 set.seed(seed = test$seed)
 smpID2 <- strata( data = tempDf, 
                   stratanames = "sub_2", 
@@ -33,11 +43,10 @@ smp <- cbind(tempDf[smpID2$ID_unit,], weight = 1 / smpID2$Prob)
 dim(smp)
 
 table(smp$sub_2)
-summary(by(smp, INDICES = smp$sub_2, function(df) {laeken::weightedMean(x = df$eqIncome, weights = df$weight)}))
+summary(by(smp, INDICES = smp$sub_2, function(df) {
+  laeken::weightedMean(x = df$eqIncome, weights = df$weight)}))
 summary(tapply(test$eusilcHHpop$eqIncome, 
                INDEX = test$eusilcHHpop$sub_2, mean))
-
-length(unique(smp$sub_2))
 
 unique(test$eusilcHHpop[c("deutsch", "sub_2")])
 unique(smp[c("deutsch", "sub_2")])
@@ -47,12 +56,14 @@ levels(test$eusilcHHpop$sub_2)
 
 smp$sub_2 <- droplevels(factor(as.character(smp$sub_2), 
                                levels = levels(test$eusilcHHpop$sub_2)))
+
 levels(smp$sub_2)
 levels(test$eusilcHHpop$sub_2)
 
+# reducing the number of variables in the dataset and
+# assigning more readable names to them
 results <- adjust_dataSets(pop = test$eusilcHHpop, smp = smp)
 
-head(smp)
 lapply(results, head)
 
 eusilcA_pop <- results$pop
